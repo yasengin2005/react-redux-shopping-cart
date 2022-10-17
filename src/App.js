4b0de873898101f587bad6c3778632a144c7167b
Engin Büküm <yasengin2005@gmail.com>
2022-10-18 00:06:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+import "./styles.css";
+import { Route } from "react-router-dom";
+
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList
+  };
+};
+
+export default connect(mapStateToProps)(App);
